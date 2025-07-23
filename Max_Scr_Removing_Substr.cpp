e735bafe6974fccd93e3e0181a99dbbc72fea13c
Kartik Manral <manralkartik09@gmail.com>
2025-07-23 23:16:00 +0530

    Added and solved maximum score from removing substring to the code base

---
+++ Max_Scr_Removing_Substr.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        if(s.size() <= 1){
+            return 0;
+        }
+        if( x < y){
+            swap(x,y);
+            reverse(s.begin(),s.end());
+        }
+        int ans = 0 , acount = 0 , bcount = 0;
+        for(char c: s){
+            if(c == 'a')acount++;
+            else if(c == 'b'){
+                if(acount> 0){
+                    acount--;
+                    ans+=x;
+                }else{
+                    bcount++;
+                }
+            }else{
+                ans+= min(acount , bcount) *y;
+                acount = bcount = 0 ;
+
+            }
+        }
+        ans+= min(acount,bcount) *y;
+        return ans;
+    }
+};
